4d5391bfee09b9ec701fa146508ede4867deac3c
Thomas Koch <tommysalsa@gmail.com>
2013-11-21 16:08:14 -0800

    trying ipad orientation fix

--- js/main.js
+++ js/main.js
@@ -23,6 +23,15 @@ function todoListClass()
 	{
 	    document.querySelector("#btnTodo").addEventListener("click", _addTodo, false);
 		document.querySelector(".formTodo").addEventListener("submit", _addTodo, false);
+		if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (viewportmeta) {
+    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+    document.body.addEventListener('gesturestart', function() {
+      viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+    }, false);
+  }
+}
 	}
 	
 	
